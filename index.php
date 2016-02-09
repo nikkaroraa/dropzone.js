f1ed4151a00c4b5a5ccb7d8601d8236b5e826a43
Niks Arora <narora200@gmail.com>
2016-02-09 10:17:42 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+ <head>
+	 <meta charset="utf-8" />
+	 <meta name="viewport" content="width=device-width, initial-scale=1" />
+	 <title>QuoteME | Welcome </title>
+	 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  	 <link rel="stylesheet" href="min/basic.min.css">
+  	 <link rel="stylesheet" href="min/dropzone.min.css">
+
+  	 <script src="min/dropzone.min.js"></script>
+  	 <script src="min/dropzone-amd-module.min.js"></script>
+</head>
+<body>
+
+<form action="file_upload.php"
+      class="dropzone"
+      id="my-awesome-dropzone">
+
+      
+
+      </form>
+</body>
+</html>
